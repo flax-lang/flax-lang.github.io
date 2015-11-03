5e808fe7c85c0ed9cdf75c93d8eee8d6552258ed
zhiayang <zhiayang@gmail.com>
2015-11-03 23:53:24 +0800

    stuff

--- js/init.js
+++ js/init.js
@@ -1,10 +1,5 @@
 $(document).ready(function()
 {
-	$(".menu-toggle").click(function(e) {
-		e.preventDefault();
-		$("#wrapper").toggleClass("toggled");
-	});
-
 	window.registerButtons = function()
 	{
 		$(".section-button").on("click", function(e)
@@ -16,7 +11,34 @@ $(document).ready(function()
 		});
 	}
 
-	Prism.highlightAll();
+	$(".menu-toggle").click(function(e)
+	{
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+
+	var currentHash = window.location.hash;
+	$(document).scroll(function()
+	{
+		$(".anchor-clicker").each(function()
+		{
+			var top = window.pageYOffset;
+			var distance = top - $(this).offset().top;
+			var hash = $(this).prev().attr("id");
+
+			// 30 is an arbitrary padding choice,
+			// if you want a precise check then use distance===0
+			if(distance < 30 && distance > -30 && currentHash != hash)
+			{
+				// window.location.hash = (hash);
+
+				// modern shit
+				window.history.pushState(null, null, '#' + hash);
+				currentHash = hash;
+			}
+		});
+	});
+
 
 	$("#sidebar-wrapper").load(window.location.origin + "/content/sidebar.html", function()
 	{
@@ -32,11 +54,17 @@ $(document).ready(function()
 		var dist = $("html, body").scrollTop() - st;
 		dist = Math.abs(dist);
 
-		var duration = Math.min(Math.max(100, dist), 500);
+		var duration = Math.min(Math.max(500, dist), 750);
 
-		$("html, body").animate({ scrollTop: st }, duration, function()
+		$("html, body").animate({ scrollTop: st }, duration, "easeOutCubic", function()
 		{
-			window.location.hash = id;
+			window.history.pushState(null, null, '#' + id);
 		});
 	});
+
+
+
+
+
+	Prism.highlightAll();
 });
