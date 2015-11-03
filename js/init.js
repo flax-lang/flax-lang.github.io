51060586764ee1aba9d476cc9571074d39cfeb71
zhiayang <zhiayang@gmail.com>
2015-11-03 23:31:50 +0800

    thing.

--- js/init.js
+++ js/init.js
@@ -23,13 +23,20 @@ $(document).ready(function()
 		registerButtons();
 	});
 
-
-
 	$(".subcontent-header").on("click", function(e)
 	{
 		e.preventDefault();
 		var id = $(this).attr("id");
+		var st = $(this).offset().top;
 
-		window.location.hash = id;
+		var dist = $("html, body").scrollTop() - st;
+		dist = Math.abs(dist);
+
+		var duration = Math.min(Math.max(100, dist), 500);
+
+		$("html, body").animate({ scrollTop: st }, duration, function()
+		{
+			window.location.hash = id;
+		});
 	});
 });
